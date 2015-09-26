d962506fb3a4cd552adfca28f9869a2ea9fc13a7
Luis Santos <luissantos@macbook-pro-de-luis.local>
2015-09-26 15:59:40 +0100

    Initial Commit

---
+++ Proj1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Proj1
+//
+//  Created by Luis Santos on 26/09/15.
+//  Copyright © 2015 Luis Santos. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}

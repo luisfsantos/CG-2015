8aba1fc9b235baf7a13a885d8341c5ef7cbfea3e
Luis Santos <luissantos@macbook-pro-de-luis.local>
2015-09-29 15:25:32 +0100

    Orange

--- Proj1/Orange.cpp
+++ Proj1/Orange.cpp
@@ -17,6 +17,14 @@ Orange::~Orange() {
 }
 
 void Orange::draw() {
-    
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glPushMatrix();
+    glTranslatef(0.0, 0.0, -1.0);
+    glRotated((GLdouble) 0, 1.0, 0.0, 0.0);
+    glDisable(GL_LIGHTING);
+    glColor3f(1.0, 0.2, 0.0);
+    glutSolidSphere(0.2, 1000, 10);
+    glEnable(GL_LIGHTING);
+    glPopMatrix();
     
 }
